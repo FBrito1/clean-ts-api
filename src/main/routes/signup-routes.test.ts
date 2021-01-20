e42451948fedcc0fb48b9a93f72669809c1ae265
Fabio Brito <fabio.britto15@gmail.com>
2021-01-20 20:30:50 -0300

    feat: ensure SignUp route returns an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('SignUp Routes', () => {
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup').send({
+        name: 'Brito',
+        email: 'brito@mail.com',
+        password: '123',
+        password_confirmation: '123'
+      })
+      .expect(200)
+  })
+})

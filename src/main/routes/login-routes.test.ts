c291958fea75c1df45a84d53ba08e9858bb50aa9
Fabio Brito <fabio.britto15@gmail.com>
2021-02-23 20:10:51 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -48,5 +48,14 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login').send({
+          email: 'fabio.brito@gmail.com',
+          password: '123123'
+        })
+        .expect(401)
+    })
   })
 })

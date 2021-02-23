b75da7586e2ba809fa86b621fe57b32900f51ab0
Fabio Brito <fabio.britto15@gmail.com>
2021-02-23 20:08:12 -0300

    feat: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,10 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -12,7 +16,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -24,6 +28,23 @@ describe('Login Routes', () => {
           email: 'fabio.brito@gmail.com',
           password: '123123',
           passwordConfirmation: '123123'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('123123', 12)
+      await accountCollection.insertOne({
+        name: 'Fabio Brito',
+        email: 'fabio.brito@gmail.com',
+        password
+      })
+      await request(app)
+        .post('/api/login').send({
+          email: 'fabio.brito@gmail.com',
+          password: '123123'
         })
         .expect(200)
     })
